ebee24f7b753b0e0f02f4e42fbab5bc4919d411b
aseckel <adam.seckel@gmail.com>
2016-12-08 14:53:39 -0800

    animate, create todo

--- client/common/column/column.controller.js
+++ client/common/column/column.controller.js
@@ -1,10 +1,14 @@
 class ColumnController {
   constructor($scope, firebaseService, safeApply) {
     const _this = this;
+    const taskHeight = 37;
+    const taskPadding = 40;
+
     this.name = 'column';
     this.$onInit = () => {
       firebaseService.getColumnTasks(this.column.key, (tasks) => {
-        this.columnTasks = tasks;
+        _this.columnTasks = tasks;
+        calculateOffset();
         safeApply($scope);
       });
     };
@@ -17,6 +21,14 @@ class ColumnController {
       this.onCreateTask(task, this.column.key);
       _this.addingTask = false;
     };
+
+    function calculateOffset() {
+      let counter = 0;
+      Object.keys(_this.columnTasks).forEach((taskKey) => {
+        _this.columnTasks[taskKey].offset = counter * (taskHeight + taskPadding);
+        counter += 1;
+      });
+    }
   }
 }
 
---
+++ todo.md
@@ -0,0 +1,7 @@
+todo
+=======
+
+- [] implement mark as completed
+- [] implement delete tasks
+- [] implement twitter auth
+- [] deploy  
